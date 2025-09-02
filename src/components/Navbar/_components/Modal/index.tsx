e2fa6e0de081c870c6af59c6e5781db93297f190
xXLuisHenriqueXx <luishenri567@gmail.com>
2025-09-02 17:49:55 -0300

    ADD: navbar component

---
+++ src/components/Navbar/_components/Modal/index.tsx
@@ -0,0 +1,76 @@
+import { motion, AnimatePresence } from "motion/react";
+import { Cpu, Factory, Settings, Sprout, Zap } from "lucide-react";
+
+interface IModalProps {
+  showMenu: boolean;
+}
+
+export const Modal = ({ showMenu }: IModalProps) => {
+  return (
+    <AnimatePresence>
+      {showMenu && (
+        <motion.nav
+          className="fixed top-15 left-4 flex flex-col items-start justify-center gap-y-4 py-6 px-4 bg-secondary rounded-sm"
+          initial={{ opacity: 0, translateY: -16 }}
+          animate={{ opacity: 1, translateY: 0 }}
+          exit={{ opacity: 0, translateY: -16 }}
+          transition={{ duration: 0.1 }}
+        >
+          <a href="#">
+            <li className="flex flex-row items-center gap-x-4 w-full px-4 py-2">
+              <Settings className="w-6 h-6 text-process" strokeWidth={2} />
+
+              <p className="text-sm font-semibold text-process">
+                Processos e Produtividade
+              </p>
+            </li>
+          </a>
+
+          <div className="w-full h-0.5 bg-background/10" />
+          <a href="#">
+            <li className="flex flex-row items-center gap-x-4 px-4 py-2">
+              <Sprout className="w-6 h-6 text-ecosystem" strokeWidth={2} />
+
+              <p className="text-sm font-semibold text-ecosystem">
+                Ambientes e Ecossistemas
+              </p>
+            </li>
+          </a>
+
+          <div className="w-full h-0.5 bg-background/10" />
+          <a href="#">
+            <li className="flex flex-row items-center gap-x-4 px-4 py-2">
+              <Zap className="w-6 h-6 text-energy" strokeWidth={2} />
+
+              <p className="text-sm font-semibold text-energy">
+                Energia e Comunicação
+              </p>
+            </li>
+          </a>
+
+          <div className="w-full h-0.5 bg-background/10" />
+          <a href="#">
+            <li className="flex flex-row items-center gap-x-4 px-4 py-2">
+              <Factory className="w-6 h-6 text-project" strokeWidth={2} />
+
+              <p className="text-sm font-semibold text-project">
+                Projeto e Fabricação
+              </p>
+            </li>
+          </a>
+
+          <div className="w-full h-0.5 bg-background/10" />
+          <a href="#">
+            <li className="flex flex-row items-center gap-x-2 px-4 py-2">
+              <Cpu className="w-6 h-6 text-tech" strokeWidth={2} />
+
+              <p className="text-sm font-semibold text-tech">
+                Tecnologia da Informação
+              </p>
+            </li>
+          </a>
+        </motion.nav>
+      )}
+    </AnimatePresence>
+  );
+};
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Menu, Sun } from "lucide-react";
+
+import { Modal } from "./_components/Modal";
+
+import LogoCT from "@assets/logo_ct.png";
+
+export const Navbar = () => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+
+  return (
+    <header className="fixed top-0 left-0 right-0 flex flex-row items-center justify-between p-4 z-50">
+      <div className="flex flex-row items-center gap-x-4">
+        <button
+          className="p-1 bg-secondary/5 rounded-sm outline-none cursor-pointer"
+          onClick={() => setShowMenu(!showMenu)}
+        >
+          <Menu className="w-6 h-6 text-secondary" />
+        </button>
+        <img className="w-12 h-6" src={LogoCT} alt="Logo CT" />
+      </div>
+
+      <Modal showMenu={showMenu} />
+
+      <button className="p-1 bg-secondary/5 rounded-full outline-none cursor-pointer">
+        <Sun className="w-6 h-6 text-secondary" />
+      </button>
+    </header>
+  );
+};
